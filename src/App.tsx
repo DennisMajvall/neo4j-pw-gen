8304434fbd3462d7cc74b3bff4be7a0c091a8d8a
Dennis Majvall <dennis.majvall@prisjakt.nu>
2025-01-12 16:44:24 +0100

    add needle dialog

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import { Dialog } from "@neo4j-ndl/react";
 
 export function App() {
   return (
     <div className="App">
-      <p>hello neo4j</p>
+      <Dialog isOpen hasDisabledCloseButton>
+        <p>hello neo4j</p>
+      </Dialog>
     </div>
   );
 }
