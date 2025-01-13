a2d07f3a893b4514294882b5874250ae214d5c50
Dennis Majvall <dennis.majvall@prisjakt.nu>
2025-01-13 18:56:35 +0100

    delete default app component

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "@neo4j-ndl/base/lib/neo4j-ds-styles.css";
 import "./index.css";
-import { App } from "./App";
+import "@neo4j-ndl/base/lib/neo4j-ds-styles.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<React.StrictMode></React.StrictMode>);
