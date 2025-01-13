2a05cc4c3bde067fdcb56b0bc56d6c27e6a9cf2e
Dennis Majvall <dennis.majvall@prisjakt.nu>
2025-01-13 18:52:38 +0100

    fix tsconfig target and add tailwind n- prefix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   presets: [require("@neo4j-ndl/base").tailwindConfig],
-  prefix: "",
   corePlugins: {
     preflight: false,
   },
